c90f56da3aca6069f44c7d73470d53ee22da2b38
m2a5x6i0m1 <max25601new@gmail.com>
2025-05-24 00:35:22 +0300

    Small adition

--- nvim/.config/nvim/lua/Neovide.lua
+++ nvim/.config/nvim/lua/Neovide.lua
@@ -19,3 +19,10 @@ vim.g.neovide_underline_stroke_scale = 1.5
 
 vim.o.winblend = 20
 vim.o.pumblend = 30
+
+vim.keymap.set("n", "<C-S-s>", ":w<CR>") -- Save
+vim.keymap.set("v", "<C-S-c>", '"+y') -- Copy
+vim.keymap.set("n", "<C-S-v>", '"+P') -- Paste normal mode
+vim.keymap.set("v", "<C-S-v>", '"+P') -- Paste visual mode
+vim.keymap.set("c", "<C-S-v>", "<C-R>+") -- Paste command mode
+vim.keymap.set("i", "<C-S-v>", '<ESC>l"+Pli') -- Paste insert mode
