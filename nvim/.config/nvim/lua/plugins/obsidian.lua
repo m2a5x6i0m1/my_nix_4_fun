0527a3dd124defe287c89d1fa91c701debd86703
m2a5x6i0m1 <max25601new@gmail.com>
2025-06-20 13:38:49 +0300

    ...

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -49,7 +49,7 @@ return {
 			},
 
 			dir = "~/notes",
-			notes_subdir = "2 Areas (Main)",
+			notes_subdir = "3 Resources",
 			new_notes_location = "notes_subdir",
 			templates = { folder = "Templates" },
 
